dda74c01b87585b7e29f8a0266047d6c91b25221
Raini Wu <rainiwu01@gmail.com>
2021-12-02 19:57:12 -0800

    wrapped Lidar grid with helper accessors

--- hdr/iface/Lidar.hpp
+++ hdr/iface/Lidar.hpp
@@ -8,6 +8,16 @@
 #include <memory>
 
 namespace LiDet {
+
+struct LiGrid : public std::array<bool, GRID_SIZE> {
+  const bool &getValue(const size_t &x, const size_t &y) {
+    return (*this)[x + y * GRID_SIZE];
+  }
+  inline void setValue(const bool &aValue, const size_t &x, const size_t &y) {
+    (*this)[x + y * GRID_SIZE] = aValue;
+  }
+};
+
 class Lidar {
 public:
   Lidar();
@@ -15,10 +25,10 @@ public:
   Lidar &operator=(const Lidar &aCopy) = delete;
   ~Lidar();
 
-  const std::array<bool, GRID_SIZE> &getGrid();
+  const LiGrid &getGrid();
 
 private:
-  std::unique_ptr<std::array<bool, GRID_SIZE>> myGrid;
+  std::unique_ptr<LiGrid> myGrid;
   std::unique_ptr<LiPkg> myLidar;
   CmdInterfaceLinux myCmdPort;
 
