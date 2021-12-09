4706979deb985b370d2fb561231a79eb18c13445
Jay Tapadiya <jay.tapadiya@gmail.com>
2021-12-08 18:48:07 -0800

    Added agentReward()

--- src/rc/Control.cu
+++ src/rc/Control.cu
@@ -7,6 +7,17 @@ void agentUpdate(float *qtable, uint8_t *cstate, uint8_t *nstate, float *reward,
 
 void agentAction(float *qtable, uint8_t *cstate, uint8_t *action) {}
 
-void agentReward(uint8_t *cstate, uint8_t *nstate, float *reward) {}
+void agentReward(uint8_t *cstate, uint8_t *nstate, float *reward) {
+    for (int i = 0; i < NUM_REGIONS; i++) {
+        if (abs(nstate[i] - CTR_STATE) != abs(cstate[i] - CTR_STATE)) { //got either closer or farther to center state
+            reward[i] = 1.0 + log(abs(nstate[i] - cstate[i])); //minimum of 1 reward, diminishing returns after
+            if (abs(nstate[i] - CTR_STATE) > abs(cstate[i] - CTR_STATE)) { //nextstate got farther from center
+                reward[i] *= -1.0;
+            }
+        } else { //state maintained
+            reward = 0.0;
+        }
+    }
+}
 
 void initvals(float *qtable) {}
