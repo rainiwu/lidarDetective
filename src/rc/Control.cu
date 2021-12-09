269691c60444d76321fa347a99a0d7f0a7ec1947
Raini Wu <rainiwu01@gmail.com>
2021-12-08 19:06:37 -0800

    implemented calcState, initvals

--- src/rc/Control.cu
+++ src/rc/Control.cu
@@ -1,6 +1,37 @@
 #include "rc/Control.cuh"
+#include <cstdlib>
 
-void calcState(uint16_t *laserDat, uint8_t *states) {}
+__global__ void findState(uint16_t *laserDat, uint8_t *states) {
+  // parallelized by region - tid is region num
+  int tid = threadIdx.x + blockIdx.x * blockDim.x;
+  int sum = 0;
+  int offset = 0;
+  int avg, tmp;
+  for (size_t i = 0; i < LIDAR_VALS / NUM_REGIONS; i++) {
+    // iterate through region
+    tmp = laserDat[tid * NUM_REGIONS + i];
+    // if bad value, discard
+    if (0 == tmp || LIDAR_MAX_V < tmp) {
+      tmp = 0;
+      offset++;
+    }
+    sum += tmp;
+  }
+  // avg of region is sum / numvals
+  avg = sum / ((LIDAR_VALS / NUM_REGIONS) - offset);
+  int stateSize = LIDAR_MAX_V / NUM_STATES;
+  // assumes target is exactly half of max v
+  for (int i = 0; i < NUM_STATES; i++) {
+    if (avg < stateSize * i) {
+      states[tid] = i + 1;
+      return;
+    }
+  }
+}
+
+void calcState(uint16_t *laserDat, uint8_t *states) {
+  findState<<<1, NUM_REGIONS>>>(laserDat, states);
+}
 
 void agentUpdate(float *qtable, uint8_t *cstate, uint8_t *nstate, float *reward,
                  uint8_t *action) {}
@@ -9,4 +40,11 @@ void agentAction(float *qtable, uint8_t *cstate, uint8_t *action) {}
 
 void agentReward(uint8_t *cstate, uint8_t *nstate, float *reward) {}
 
-void initvals(float *qtable) {}
+__global__ void initQtable(float *qtable) {
+  int tid = threadIdx.x + blockIdx.x * blockDim.x;
+  qtable[tid] = 0;
+}
+void initvals(float *qtable) {
+  size_t qtabSize = (NUM_STATES ^ NUM_REGIONS) * NUM_ACTION;
+  initQtable<<<qtabSize / 64, 64>>>();
+}
